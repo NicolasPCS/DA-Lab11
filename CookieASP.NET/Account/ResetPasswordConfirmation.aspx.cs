dc804f1d9a517d56ee275d77753f53ce689fe3ff
Nicolas Caytuiro <nicolascaytuirosilva@gmail.com>
2021-11-22 13:55:08 -0500

    Implementacion de Cookie ASP.NET

---
+++ CookieASP.NET/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace CookieASP.NET.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ CookieASP.NET/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CookieASP.NET.Startup))]
+namespace CookieASP.NET
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
