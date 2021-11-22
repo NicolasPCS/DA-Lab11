f8a03e963bbed5b94e1e52f6e0f057046c1f6526
Nicolas Caytuiro <nicolascaytuirosilva@gmail.com>
2021-11-22 14:12:15 -0500

    Implementacion del Cookie Collection

---
+++ CookieCollection/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CookieCollection.Startup))]
+namespace CookieCollection
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
