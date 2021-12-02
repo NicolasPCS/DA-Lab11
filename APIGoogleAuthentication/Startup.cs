6731affff205cb29c222bb7b52dd57fa443967d3
Nicolás <nicolascaytuirosilva@gmail.com>
2021-12-02 13:47:08 -0500

    Se culmino con la implementación de la experiencia 2 (subido con retraso por no tener a los archivos del proyecto)

---
+++ APIGoogleAuthentication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(APIGoogleAuthentication.Startup))]
+namespace APIGoogleAuthentication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
